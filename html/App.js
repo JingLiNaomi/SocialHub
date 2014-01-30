9f89ebbb368b5651e7c4cad10f0b38fee12dd2ac
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2014-01-30 14:35:29 +0000

    updated layout

---
+++ html/App.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+
+console.log('Ember loaded');
+App = Ember.Application.create();
---
+++ js/App.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+	this.resource('myfriends');
+});
+	

0145f043254605bb22bdaf0812b72a3bd482d80d
Daniyar <d.kaliyev@cs.ucl.ac.uk>
2014-02-02 11:00:31 +0000

    fixed popup

--- js/libs/script.js
+++ js/libs/script.js
@@ -1,54 +1,39 @@
-jQuery(function($){
 
-$("a.topopup").click(function() {
-loading();
+ topopupClicked = function() {
+//loading();
 setTimeout(function(){
 loadPopup();
 },500);
 return false;
-});
+}
 
 
 /* event for close the popup */ 
 
-$("a.clickcross").click(function() {
+crossClicked = function() {
 	disablePopup();
-});
-$("div.close").click(function() { 
-disablePopup(); // function close pop up 
-}); 
-$(this).keyup(function(event) { 
-if (event.which == 27) { // 27 is 'Ecs' in the keyboard 
-disablePopup(); // function close pop up 
-} 
-}); 
-$("div#backgroundPopup").click(function() { 
-disablePopup(); // function close pop up 
-}); 
-$("div#cross").click(function() {
-	disablePopup();
-});
+}
 
- 
+
  /************** start: functions. **************/ 
- function loading() { 
+loading = function() { 
  $("div.loader").show(); 
  } 
- function closeloading() { 
+closeloading = function () { 
  $("div.loader").fadeOut('normal'); 
  } 
  
  var popupStatus = 0; // set value 
- function loadPopup() { 
+ loadPopup = function(){ 
  if(popupStatus == 0) { // if value is 0, show popup 
- closeloading(); // fadeout loading 
- $("#toPopup").fadeIn(0500); // fadein popup div 
+ //closeloading(); // fadeout loading 
+ $("#toPopup").fadeIn(500); // fadein popup div 
  $("#backgroundPopup").css("opacity", "0.7"); // css opacity, supports IE7, IE8 
  $("#backgroundPopup").fadeIn(0001); 
  popupStatus = 1; // and set value to 1 
  } 
  } 
- function disablePopup() { 
+disablePopup = function() { 
  if(popupStatus == 1) { // if value is 1, close popup 
  $("#toPopup").fadeOut("normal"); 
  $("#backgroundPopup").fadeOut("normal"); 
@@ -56,4 +41,3 @@ $("div#cross").click(function() {
  } 
  } 
  /************** end: functions. **************/ 
- }); // jQuery End

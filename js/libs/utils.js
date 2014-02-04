d11f6542b29fb22a8e41f13c6bff64d4e53f298c
businesscloud <business_cloud@hotmail.com>
2014-02-04 18:43:24 +0000

    fixed tabs

--- js/libs/utils.js
+++ js/libs/utils.js
@@ -3,6 +3,18 @@ toggle = function(id){
 	$('#'+id).slideToggle("fast");
 }
 
+
+
+function activate(id)
+{
+	$('.tab').removeClass("active");
+	$("#"+id).addClass("active");
+	return true;
+}
+
+	
+
+	
 popupClick = function(id)
 {
 	
